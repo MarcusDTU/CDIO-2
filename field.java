ab12763fa5d2538c027d3e0c929386ec5287e4e2
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-10 12:10:23 +0200

    Method extraTurn has been created

--- field.java
+++ field.java
@@ -1,4 +1,13 @@
 class field{
     int [] money = {250, -100, 100,-20, 180, 0, -70, 60, -80, -50, 650};
-    
+
+
+    public static boolean extraTurn(){
+        boolean extraTurn = false;
+
+
+
+
+        return extraTurn;
+    }
 }
