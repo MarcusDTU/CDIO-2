066ed1871815c5c62bcef8be07b82ba7ecd7988f
davidwellejus <davidwellejus@gmail.com>
2023-10-10 16:04:16 +0200

    First take on account

---
+++ Account.java
@@ -0,0 +1,13 @@
+class Account {
+    public int id;
+    private int cashBalance;
+    
+    public Account(int id) {
+        this.id = id;
+    }
+
+    public int getCashBalance() {
+        return this.cashBalance;
+    }
+
+}
