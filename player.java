df00d65404dadfa624be067f23367006e66a8c86
Klavs <s213383@dtu.dk>
2023-10-11 20:17:43 +0200

    Jeg har kodet noget godt i player branch

---
+++ player.java
@@ -0,0 +1,15 @@
+public class Player {
+    private String name;
+    private int ID;
+    public int account;
+
+    public Player(String name int ID) {
+        this.name = name;
+        this.ID = ID;
+        this.account = account(ID)
+    }
+
+    public String getName() {
+        return name;
+    }
+}
