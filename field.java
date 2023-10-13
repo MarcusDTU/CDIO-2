5c2b42bca45542f4c4d1ea787f94e5f79d9251f5
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-13 14:30:31 +0200

    added comments to the code

--- field.java
+++ field.java
@@ -3,6 +3,7 @@ class Field{
 
 
     public static int fieldPoint(int [] dieArray){
+        /*check witch field the player rolled and return the money cooresponding to the dice sum */
         int balanceChange; 
         int [] money = {250, -100, 100,-20, 180, 0, -70, 60, -80, -50, 650};
         balanceChange = money[dieArray[2]];
@@ -10,6 +11,7 @@ class Field{
     }
 
     public boolean extraTurn(int []dieArray){
+        /*Checks if the dice sum is 10 and then return a true value */
         boolean extraTurn = false;
 
         
