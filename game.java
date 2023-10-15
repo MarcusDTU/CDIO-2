70cc2ccaf56246769d7e6ad6321f4a43720750de
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 13:54:58 +0200

    get the wanted language for the game

--- game.java
+++ game.java
@@ -1,14 +1,22 @@
+
 class game{
     public static void main(String[] args){
         //create scanner obj
         var scanner = new java.util.Scanner(System.in);
         scanner.useLocale(java.util.Locale.ENGLISH);
+        String newline = System.lineSeparator();
 
-        System.out.println("Choose language");
-
-        var language = scanner.nextLine();
+        System.out.println("Choose language" + newline + "1 for English" + newline + "2 for Danish");
         
-
+        var language = scanner.nextInt();
+        if(language == 1){
+            String gameLanguage = "english";
+        }
+        
+        scanner.close();
+        
+       
+        
 
     }
 }
