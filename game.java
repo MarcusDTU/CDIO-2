b43e45a20b3101910cde98ee2176f3bd75a53145
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 16:43:14 +0200

    added comments

--- game.java
+++ game.java
@@ -2,6 +2,8 @@ class game{
     public static void main(String[] args){
 
         languageselecter gameLanguage = new languageselecter();
+        
+        //gameLanguageForMsg is the string that has beed saved from the languageSetup method and returns the selected language for later use in messege class
         var gameLanguageForMsg = gameLanguage.languageSetup();
 
        
