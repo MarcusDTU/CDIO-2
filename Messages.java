4cd3b7c69659af03b661cafb796715b2393d4792
Nikolaj <Nikolaj.Schaebel@gmail.com>
2023-10-15 10:28:03 +0200

    Added the function getTranslatedDescription. It's used to generate a description for a Message object, based on a language (string) and the object's messageClass and id. Currently, the descriptions are just empty strings.

--- Messages.java
+++ Messages.java
@@ -4,4 +4,70 @@ public class Message {
     private int messageClass;
     private String description;
 
+    private String getTranslatedDescription(String language, Message message) {
+        if (language.equals("english")) {
+            // messageClass 0 is for system messages.
+            if (this.messageClass == 0) {
+                if (this.id == 0) {
+                    this.description = "";
+                }
+            }
+            // messageClass 1 is for game-related messages that do not belong to either roll
+            // or die.
+            if (this.messageClass == 1) {
+                if (this.id == 0) {
+                    this.description = "";
+                }
+            }
+            // messageClass 2 is for roll-related messages.
+            if (this.messageClass == 2) {
+                if (this.id == 0) {
+                    this.description = "";
+                }
+            }
+            // messageClass 3 is for field-related messages, where the id matches the sum of
+            // the roll.
+            if (this.messageClass == 3) {
+                if (this.id == 0) {
+                    this.description = "";
+                }
+                if (this.id == 1) {
+                    this.description = "";
+                }
+                if (this.id == 2) {
+                    this.description = "";
+                }
+                if (this.id == 3) {
+                    this.description = "";
+                }
+                if (this.id == 4) {
+                    this.description = "";
+                }
+                if (this.id == 5) {
+                    this.description = "";
+                }
+                if (this.id == 6) {
+                    this.description = "";
+                }
+                if (this.id == 7) {
+                    this.description = "";
+                }
+                if (this.id == 8) {
+                    this.description = "";
+                }
+                if (this.id == 9) {
+                    this.description = "";
+                }
+                if (this.id == 10) {
+                    this.description = "";
+                }
+                if (this.id == 11) {
+                    this.description = "";
+                }
+                if (this.id == 12) {
+                    this.description = "";
+                }
+            }
+        }
+    }
 }
