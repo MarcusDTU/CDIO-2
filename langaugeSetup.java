61dbd2d0ec6b02dfa2b9c58d105cd7adbf7d4b1d
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 14:19:57 +0200

    languageSetup is moved to its own java file

---
+++ langaugeSetup.java
@@ -0,0 +1,31 @@
+class language{
+    public String languageSetup(){
+        boolean languageFound = false;
+        //create scanner obj
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+        String newline = System.lineSeparator();
+
+        
+        while(!languageFound){
+            System.out.println("Choose language:" + newline + "1 for English" + newline + "2 for Danish");
+        
+            var language = scanner.nextLine();
+        
+            //check what language has been choosen
+            if(language.equals("1")){
+                String gameLanguage = "english";
+                System.out.println("English has been choosen");
+                languageFound = true;
+            }
+            else{
+                System.out.println("Language not supported"+newline);
+
+            }
+
+        
+        }
+        scanner.close();
+        return gameLanguage;
+    }
+}
