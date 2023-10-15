7c12e2a8a57c5126e98d343bda5ea826e57bb689
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 10:01:02 +0200

    Change the name of the die array to match Asmas

--- field.java
+++ field.java
@@ -2,20 +2,20 @@ class Field{
     
 
 
-    public static int fieldPoint(int [] dieArray){
+    public static int fieldPoint(int [] rolls){
         /*check witch field the player rolled and return the money cooresponding to the dice sum */
         int balanceChange; 
         int [] money = {250, -100, 100,-20, 180, 0, -70, 60, -80, -50, 650};
-        balanceChange = money[dieArray[2]];
+        balanceChange = money[rolls[2]];
         return balanceChange;
     }
 
-    public boolean extraTurn(int []dieArray){
+    public boolean extraTurn(int []rolls){
         /*Checks if the dice sum is 10 and then return a true value */
         boolean extraTurn = false;
 
         
-        if(dieArray[2] == 10){
+        if(rolls[2] == 10){
             extraTurn = true;
             
         }
