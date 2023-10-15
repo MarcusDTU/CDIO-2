38e3b8eb218bdfd4820f1faa0f6fbb0b0c05fd62
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 13:20:14 +0200

    scanner added

--- game.java
+++ game.java
@@ -1,5 +1,14 @@
 class game{
     public static void main(String[] args){
+        //create scanner obj
+        var scanner = new java.util.Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+
+        System.out.println("Choose language");
+
+        var language = scanner.nextLine();
         
+
+
     }
 }
