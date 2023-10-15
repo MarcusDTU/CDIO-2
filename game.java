ef6f16783d54389a9a0c02619c0f34368bef0c0e
MarcusDTU <142238430+MarcusDTU@users.noreply.github.com>
2023-10-15 17:04:27 +0200

    created the 2 players

--- game.java
+++ game.java
@@ -6,6 +6,15 @@ class game{
         //gameLanguageForMsg is the string that has beed saved from the languageSetup method and returns the selected language for later use in messege class
         var gameLanguageForMsg = gameLanguage.languageSetup();
 
+        Message message = new Messege();
+        
+        //sets game language from the gameLanguageForMsg string
+       message.setLanguage(gameLanguageForMsg);
+
+
+       //creating the to players 
+       Player Player1 = new Player("test1", 1);
+       Player Player2 = new Player("test2", 2);
        
         
 
