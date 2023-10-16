8fc7e7ec499b1cd099b932445e253de2864df948
davidwellejus <davidwellejus@gmail.com>
2023-10-16 11:22:36 +0200

    added messages for dice values

--- game.java
+++ game.java
@@ -30,8 +30,9 @@ class game{
             while (!(roll.equals(""))) {
                 System.out.println(messages[2][0]); //Press enter to roll die.
             }
-            int[] rolls = rollDie();
-            System.out.println(messages[2][1] + " " + rolls[0] + System.lineSeparator() + messages[2][2] + "" + rolls[1] + System.lineSeparator() + messages[2][3] + "" + rolls[2]);
+            int[] rolls = die.rollDie();
+            System.out.println(messages[2][1] + "" + rolls[0] + System.lineSeparator() + messages[2][2] + "" + rolls[1] + System.lineSeparator() + messages[2][3] + "" + rolls[2]);
+            // 2.1 = The value of die one is:, 2.2 = The value of die two is:, 2.3 = Total sum of dice is:
         }
     }
 }
