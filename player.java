41cc6ad9ab21cd024cfee7d963eead6873bf2722
davidwellejus <davidwellejus@gmail.com>
2023-10-16 10:12:57 +0200

    Rescued Marcus from total disaster

--- player.java
+++ player.java
@@ -1,15 +1,34 @@
 class Player {
     private String name;
-    public int ID;
-    public Account account;
+    private int id;
+    private Account account;
 
-    public Player(String name, int ID) {
+    public Player(String name, int id) {
         this.name = name;
-        this.ID = ID;
-        this.account = new Account(ID);
+        this.id = id;
+        this.account = new Account(id);
+    }
+
+    public static Player createPlayer(int i) {
+        // Taking the names of players.
+        var scannerCreate = new java.util.Scanner(System.in);
+        System.out.println("Please enter the name of player " + i + ": ");
+        var name = scannerCreate.nextLine();
+        Player player = new Player(name, i);
+        return player; 
     }
 
     public String getName() {
         return name;
+    }
+    public int getAccountBalance() {
+        return this.account.getCashBalance();
+    }
+
+    public int getAccountId() {
+        return this.id;
+    }
+    public void setCashBalance(int money) {
+        this.account.setCashBalance(money);
     }
 }
