bc4a84d882c82925dc4a6fbbfd9f959182aac561
davidwellejus <davidwellejus@gmail.com>
2023-10-16 12:08:02 +0200

    Added game flow for player2

--- game.java
+++ game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class game{
     public static void main(String[] args){
-
+        boolean winnerFound = false;
         languageselecter gameLanguageObj = new languageselecter();
         
         //gameLanguageForMsg is the string that has beed saved from the languageSetup method and returns the selected language for later use in messege class
@@ -24,7 +24,7 @@ class game{
         System.out.println(messages[1][0]); //Players will take turns, rolling two dice to move to a field labeled with numbers from 2 to 12. Each field can either increase or decrease the player's money. All players start with a 1000 balance, and the winner is the first to reach 3000.
         
         //Game runs in a while loop which checks if a winner has been found. If not the game will continue to ask players to roll dice
-        while(!winnerFound()) {
+        while(!winnerFound) {
             System.out.println(messages[2][0]); //Press enter to roll die.
             var roll = Scanner.nextLine("");
             while (!(roll.equals(""))) {
@@ -42,6 +42,41 @@ class game{
             System.out.println(messages[3][rolls[2]]);
             var updatedCashBalance = Player1.getPlayerAccountBalance();
             System.out.println(messages[1][1] + "" + updatedCashBalance); //Your updated cash balance is:
+
+            //check if player1 has won
+            winnerFound = getWinCon(Player1);
+
+            // if the winner has been found it break out of the while loop
+            if(winnerFound) {
+                break;
+            }
+
+            System.out.println(messages[2][0]); //Press enter to roll die.
+            roll = Scanner.nextLine("");
+            while (!(roll.equals(""))) {
+                System.out.println(messages[2][0]); //Press enter to roll die.
+            }
+            rolls = die.rollDie();
+            System.out.println(messages[2][1] + "" + rolls[0] + System.lineSeparator() + messages[2][2] + "" + rolls[1] + System.lineSeparator() + messages[2][3] + "" + rolls[2]);
+            // 2.1 = The value of die one is:, 2.2 = The value of die two is:, 2.3 = Total sum of dice is:
+
+            //Gets the field value and sets  player's cash balance 
+            points = fieldPoint(rolls[2]);
+            Player2.setPlayerCashBalance(points);
+
+            //Prints field info and updated player account balance
+            System.out.println(messages[3][rolls[2]]);
+            updatedCashBalance = Player1.getPlayerAccountBalance();
+            System.out.println(messages[1][1] + "" + updatedCashBalance); //Your updated cash balance is:
+
+            //check if player1 has won
+            winnerFound = getWinCon(Player2);
+            
+            // if the winner has been found it break out of the while loop
+            if(winnerFound) {
+                break;
+            }
+
         }
     }
 }
