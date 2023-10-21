acb1c00faa7ff1c72405cdfc2989de4e1e90440f
Nikolaj Schæbel <s220471@dtu.dk>
2023-10-21 16:21:52 +0200

    Added field messages to messageClass 3.

--- Messages.java
+++ Messages.java
@@ -148,37 +148,37 @@ class Message {
                     returnDescription = "";
                 }
                 if (id == 2) {
-                    returnDescription = "2";
+                    returnDescription = "Tower: You have found a lone tower surrounded by lava. You decide to investigate the tower, only to stumble upon a sleeping princess. You walk towards her and start shaking her violently. She gets quite mad and commands you to slay the dragon that guards her tower. After you have slayed the dragon, the princess offers you some gold, but the princess is a greedy c*** and only gives you 250 gold. You think to yourself: “What a horribly woman.”";
                 }
                 if (id == 3) {
-                    returnDescription = "3";
+                    returnDescription = "Crater: On your way back home, you get pre-occupied with thinking about how awesome it is to be a noble.  Due to your dangerous lack of awareness of your surroundings, you end up falling into a big crater, where you are trapped. Luckily, a princess is nearby to help you up. After you got up from the pit, the princess demands that you pay her 100 gold for the rescue mission. ";
                 }
                 if (id == 4) {
-                    returnDescription = "4";
+                    returnDescription = "Palace gates: You finally got home from your mission, but in the meantime, someone decided to build a wall and gate around your palace. The guards won’t let you in. You demand to be let in, since it is you home. The guards then realize who you are and therefore they must pay a fine of 100 gold. Still not satisfied, you place the guards and their families in the stockades. Being a noble is freaking awesome.";
                 }
                 if (id == 5) {
-                    returnDescription = "5";
+                    returnDescription = "Cold desert: You are going on an adventure and find yourself in a desert in the middle of the night. It is a cold and storm full night, and you are about to freeze to death. Luckily, you find a merchant. He sells you a cup of warm chocolate for 20 gold and you survive the night. The chocolate did not even include whipped cream. Bummer.";
                 }
                 if (id == 6) {
-                    returnDescription = "6";
+                    returnDescription = "Walled city:  You stumble upon a walled city. When trying to enter the city, the mayor stops you and congratulates you for being visitor number 1,000,000. You receive the grand price of 180 gold.";
                 }
                 if (id == 7) {
-                    returnDescription = "7";
+                    returnDescription = "Monastery: You want to get drunk beyond any recognition and you have heard rumors about the monks at the monastery brewing some strong beer. The monks are very generous and don’t want any gold for all the beer you drank.  You end the night sleeping in the mud with a smile on your face. You consider establishing a monastery in your realms for a steady supply of free beer.";
                 }
                 if (id == 8) {
-                    returnDescription = "8";
+                    returnDescription = "Black cave: You entered a dark cave. All the sudden you hear a voice behind you. “Give me all your gold” says the thief. However, you are a master negotiator and end up paying 80 gold plus your underpants to the thief. You really miss your underpants due to the chaffing of your chainmail.";
                 }
                 if (id == 9) {
-                    returnDescription = "9";
+                    returnDescription = "Huts in the mountain: While hiking in the mountain, you hear loud music playing. While following the music, you find a hut, where some students from DTU are holding an Après Ski party.  Some mechanical engineering student challenge you to a beer pong tournament. After destroying them, you win 60 gold. 60 gold doesn’t seem as much, but then you remember that the students are poor. You temporarily consider a career as a professional beer pong player. ";
                 }
                 if (id == 10) {
-                    returnDescription = "10";
+                    returnDescription = "The Werewall:  In Eldoria's Thicket of Shadows stands the Werewall, a living barrier made from spirits of ancient werewolves. Created for protection, it shifts and howls, granting passage only to those with pure intent. You pay the toll of 80 gold. But hey, you can continue your adventure (extra turn).";
                 }
                 if (id == 11) {
-                    returnDescription = "11";
+                    returnDescription = "The pit:  In the fighting pit, where blood and gore are a regular sight. Your gladiator is about to fight the reigning champion. You are tipped the judged in your favor and therefore bet 50 gold. But unfortunately, your gladiator slipped and beheaded himself. You lost your 50 gold.";
                 }
                 if (id == 12) {
-                    returnDescription = "12";
+                    returnDescription = "Goldmine: After years of living with the pigs in their stables. Eating nothing else but leftovers from the farmer. One night you heard the farmer and his wife talking about a legendary goldmine where there is enough gold for a lifetime. One day a mysteries man comes to the farm and tells you about the goldmine.  It turns out the goldmine is only a few miles away. You decide to go on an adventure and found to goldmine. The story about gold enough for a lifetime turns out to be a lie and there was only gold enough for a couple of years. You collected 650 gold and put them in your pants, which now weight more than you mom.";
                 }
             }
         }
@@ -213,7 +213,16 @@ class Message {
 // id 3: Total sum of dice is:(space after ": ")
 // ...
 // FIELD MESSAGES:
-// id 0:
-// id 1:
-// id 2:
-// ...
+// id 0: 
+// id 1: 
+// id 2: Tower: You have found a lone tower surrounded by lava. You decide to investigate the tower, only to stumble upon a sleeping princess. You walk towards her and start shaking her violently. She gets quite mad and commands you to slay the dragon that guards her tower. After you have slayed the dragon, the princess offers you some gold, but the princess is a greedy c*** and only gives you 250 gold. You think to yourself: “What a horribly woman.”
+// id 3: Crater: On your way back home, you get pre-occupied with thinking about how awesome it is to be a noble.  Due to your dangerous lack of awareness of your surroundings, you end up falling into a big crater, where you are trapped. Luckily, a princess is nearby to help you up. After you got up from the pit, the princess demands that you pay her 100 gold for the rescue mission. 
+// id 4: Palace gates: You finally got home from your mission, but in the meantime, someone decided to build a wall and gate around your palace. The guards won’t let you in. You demand to be let in, since it is you home. The guards then realize who you are and therefore they must pay a fine of 100 gold. Still not satisfied, you place the guards and their families in the stockades. Being a noble is freaking awesome.
+// id 5: Cold desert: You are going on an adventure and find yourself in a desert in the middle of the night. It is a cold and storm full night, and you are about to freeze to death. Luckily, you find a merchant. He sells you a cup of warm chocolate for 20 gold and you survive the night. The chocolate did not even include whipped cream. Bummer.
+// id 6: Walled city:  You stumble upon a walled city. When trying to enter the city, the mayor stops you and congratulates you for being visitor number 1,000,000. You receive the grand price of 180 gold.
+// id 7: Monastery: You want to get drunk beyond any recognition and you have heard rumors about the monks at the monastery brewing some strong beer. The monks are very generous and don’t want any gold for all the beer you drank.  You end the night sleeping in the mud with a smile on your face. You consider establishing a monastery in your realms for a steady supply of free beer.
+// id 8: Black cave: You entered a dark cave. All the sudden you hear a voice behind you. “Give me all your gold” says the thief. However, you are a master negotiator and end up paying 80 gold plus your underpants to the thief. You really miss your underpants due to the chaffing of your chainmail.
+// id 9: Huts in the mountain: While hiking in the mountain, you hear loud music playing. While following the music, you find a hut, where some students from DTU are holding an Après Ski party.  Some mechanical engineering student challenge you to a beer pong tournament. After destroying them, you win 60 gold. 60 gold doesn’t seem as much, but then you remember that the students are poor. You temporarily consider a career as a professional beer pong player. 
+// id 10: The Werewall:  In Eldoria's Thicket of Shadows stands the Werewall, a living barrier made from spirits of ancient werewolves. Created for protection, it shifts and howls, granting passage only to those with pure intent. You pay the toll of 80 gold. But hey, you can continue your adventure (extra turn).
+// id 11: The pit:  In the fighting pit, where blood and gore are a regular sight. Your gladiator is about to fight the reigning champion. You are tipped the judged in your favor and therefore bet 50 gold. But unfortunately, your gladiator slipped and beheaded himself. You lost your 50 gold.
+// id 12: Goldmine: After years of living with the pigs in their stables. Eating nothing else but leftovers from the farmer. One night you heard the farmer and his wife talking about a legendary goldmine where there is enough gold for a lifetime. One day a mysteries man comes to the farm and tells you about the goldmine.  It turns out the goldmine is only a few miles away. You decide to go on an adventure and found to goldmine. The story about gold enough for a lifetime turns out to be a lie and there was only gold enough for a couple of years. You collected 650 gold and put them in your pants, which now weight more than you mom.
