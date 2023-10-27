13da95a0da67065d01524c2efb7aaba09ef9e7b5
salehvidovre <s110779@dtu.dk>
2023-10-27 12:21:07 +0200

    fuck this

--- CDIO-2/src/Accounttest.java
+++ CDIO-2/src/Accounttest.java
@@ -1,11 +1,26 @@
-
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class AccountTest {
-    Player Player1 = new Player("Test", 1);
+    Player player1 = new Player("Test", 1);
+
+
     @Test
     public void setPlayerCashBalanceTest(){
-        Player1.setPlayerCashBalance(-3000);
-        assertEquals(0,Player1.getPlayerAccountBalance());
+        player1.setPlayerCashBalance(-3000);
+        assertEquals(0,player1.getPlayerAccountBalance());
+
+        player1.setPlayerCashBalance(1000);
+        
+
+        player1.setPlayerCashBalance(0);       
+        assertEquals(1000, player1.getPlayerAccountBalance());
+    
+        player1.setPlayerCashBalance(3000);
+        assertEquals(4000, player1.getPlayerAccountBalance());
+    
     }
+
+
+
+
 }
